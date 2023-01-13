9bdf09763f5eb724a63aecdeffb6f12bd66f91ad
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2023-01-13 13:28:26 -0700

    commit tree 89fb846427ce79068814aabe1d9a8f6aa9616b2c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]} # %w("Fiction"), %w("Non-Fiction")
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? {|pattern| pattern.match title}
+            errors.add(:title, "must be clickbait")
+            #dont need else because they're on their own lines
+        end
+    end
 end
